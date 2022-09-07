d6eab75d83b5448c21b6977f655f225829206348
Kishan Mandora <kishanmandora7@gmail.com>
2022-09-08 01:49:32 +0530

    add 6th exercise 2nd extra credit solution

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,9 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = React.useRef()
+  const [petname, setPetname] = React.useState('')
+
+  const checkLowerCase = petname !== petname.toLowerCase()
 
   function submitHandler(e) {
     e.preventDefault()
@@ -13,16 +16,55 @@ function UsernameForm({onSubmitUsername}) {
     console.log(e.target.elements.surname.value, 'is surname')
   }
 
+  const displayFlex = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+
   return (
-    <form onSubmit={submitHandler}>
-      <div>
-        <label>Username:</label>
-        <input type="text" ref={inputRef} />
-        <label htmlFor="surname">Surname:</label>
-        <input name="surname" id="surname" type="text" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div
+      style={{
+        margin: 0,
+        padding: 0,
+        width: '100%',
+        ...displayFlex,
+      }}
+    >
+      <form onSubmit={submitHandler}>
+        <div
+          style={{
+            margin: '10px auto',
+            padding: 0,
+            width: '30%',
+            ...displayFlex,
+          }}
+        >
+          <label>Username:</label>
+          <input type="text" ref={inputRef} />
+          <br />
+          <label htmlFor="surname">Surname:</label>
+          <input name="surname" id="surname" type="text" />
+          <br />
+          <label htmlFor="petname">Petname:</label>
+          <input
+            name="petname"
+            id="petname"
+            type="text"
+            value={petname}
+            onChange={e => setPetname(e.target.value)}
+          />
+          {checkLowerCase && (
+            <span style={{color: 'red'}}>
+              <b> Petname should be lowercase </b>
+            </span>
+          )}
+          <button disabled={checkLowerCase} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
